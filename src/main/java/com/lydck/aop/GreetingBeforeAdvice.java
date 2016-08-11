4ef1851d2fdfa2ddfa62ba1dca37b0527f226b1f
Lydck <1468390049@qq.com>
2016-08-11 08:41:59 +0800

    DefaultAdvisorAutoProxyCreator扫描容器中的Advisor，并为他们自动创建代理类

--- src/main/java/com/lydck/aop/GreetingBeforeAdvice.java
+++ src/main/java/com/lydck/aop/GreetingBeforeAdvice.java
@@ -8,7 +8,7 @@ public class GreetingBeforeAdvice implements MethodBeforeAdvice {
 
 	@Override
 	public void before(Method method, Object[] args, Object target) throws Throwable {
-		System.out.println(target.getClass().getName() + "." + method.getName());
+		System.out.println("增强：" + target.getClass().getName() + "." + method.getName());
 		String clientName = (String) args[0];
 		System.out.println("How are you! Mr." + clientName);
 	}
