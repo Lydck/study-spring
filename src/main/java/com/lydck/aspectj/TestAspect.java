5f4b3829809b1fc28fdc0453b8dcae761856c662
Lydck <1468390049@qq.com>
2016-08-24 01:19:44 +0800

    连接点方法入参和签名信息

--- src/main/java/com/lydck/aspectj/TestAspect.java
+++ src/main/java/com/lydck/aspectj/TestAspect.java
@@ -1,12 +1,39 @@
 package com.lydck.aspectj;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class TestAspect {
 	@AfterReturning("@annotation(com.lydck.aspectj.NeedTest) and execution(* greetTo(..))")
 	public void needTestFun() {
 		System.out.println("needTestFun() executed!");
+	}
+	
+	@After("within(com.lydck.aspectj.*) && execution(* greetTo(..))")
+	public void greetToFun() {
+		System.out.println("greetToFun() executed!");
+	}
+	
+	@Before("!target(com.lydck.aspectj.NaiveWaiter) && execution(* serviceTo(..))")
+	public void notServeinNaiveWaiter() {
+		System.out.println("notServeinNaiveWaiter() executed!");
+	}
+	
+	@AfterReturning("target(com.lydck.aspectj.Waiter) || target(com.lydck.aspectj.Seller)")
+	public void waiterOrSeller() {
+		System.out.println("waiterOrSeller() executed!");
+	}
+	
+	@Around("execution(* greetTo(..))")
+	public void joinPointAccess(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("------joinPointAccess-------");
+		System.out.println("arg[0]: " + pjp.getArgs()[0]);
+		System.out.println("signature: " + pjp.getSignature().toString());
+		pjp.proceed();
 	}
 }
