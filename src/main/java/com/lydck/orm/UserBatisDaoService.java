60e238a77665a1fb8c5d3396bf78a0cbe9827c22
Lydck <1468390049@qq.com>
2016-09-20 02:57:06 +0800

    myBatis添加动态查询

--- src/main/java/com/lydck/orm/UserBatisDaoService.java
+++ src/main/java/com/lydck/orm/UserBatisDaoService.java
@@ -1,5 +1,18 @@
 package com.lydck.orm;
 
-public class UserBatisDaoService {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.lydck.domain.User;
+
+@Service("userBatisDaoService")
+public class UserBatisDaoService {
+	
+	@Autowired
+	private SqlSessionTemplate batisTemplate;
+	
+	public User getUser(User user) {
+		return batisTemplate.selectOne(UserBatisDaoService.class.getName() + ".getUser", user);//com.lydck.orm.UserBatisDaoService
+	}
 }
